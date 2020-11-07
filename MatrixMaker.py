e9667834e8cfe4b46312e69e4a692d5418354e84
Sebastian1819 <42974667+Sebastian1819@users.noreply.github.com>
2020-11-07 15:51:34 +0100

    Update MatrixMaker.py

--- MatrixMaker.py
+++ MatrixMaker.py
@@ -2,33 +2,43 @@ import sys
 import struct
 
 #this function reads the test file and assigns it the variables and makes the adjencecy matrix for the algoirithm to follow
+
 def MatrixMaker():
 
-    file = open('samples-A/2.in', 'r')
+    file = open('samples-A/1.in', 'r')
     #file = sys.stdin
 
     cities, roads = map(int,file.readline().split())
-    print('cities: '+str(cities)+' roads: '+str(roads))
 
     adArray = [[0 for i in range(cities)] for j in range(cities)]
 
-    sourceNum , sinNum = map(int,file.readline().split())
+    sourceNum , sinkNum = map(int,file.readline().split())
 
-    sourceArray = file.readline()
-    sinkArray = file.readline()
-
-    print(adArray)
-    print(sourceArray,end='')
-    print(sinkArray,end='')
+    sourceArray = file.readline().split()
+    sinkArray = file.readline().split()
 
     for i in range(int(roads)):
         a,b,c = map(int,file.readline().split())
         adArray[a][b] = c
 
-    print(adArray)
-
     file.close()
 
+    return adArray,sourceNum,sinkNum,sourceArray,sinkArray
+
+def BSF(adArray,queue,sinkArray):
+    newQueue = []
+    for node in queue:
+        print(node)
+        i=0
+        for edge in adArray[int(node)]:
+            if edge>0:
+                newQueue.append(i)
+            i+=1
+    print(newQueue)
+    if newQueue.__len__()>0:
+        BSF(adArray,newQueue,sinkArray)
 
 if __name__ == "__main__":
-    MatrixMaker()
+    adArray, sourceNum, sinNum, sourceArray, sinkArray = MatrixMaker()
+    print(adArray)
+    BSF(adArray,sourceArray,sinkArray)
